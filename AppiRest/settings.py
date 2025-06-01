899a99d2c904d428749a88aa7039089dd2630196
andresvils <fenixblanco789@gmail.com>
2025-05-31 23:04:48 -0300

    dios mio god

--- AppiRest/settings.py
+++ AppiRest/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-98#&t^r+mfowvs-1pp05i=6hsnj%*@ujzlyn^n(8cy+s_^pqu4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://test-roe3.onrender.com"]
-
+ALLOWED_HOSTS = ["test-roe3.onrender.com", "localhost"]
 
 # Application definition
 
